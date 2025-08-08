eaf0a97703844c1b6b3d7c533ee26677e877cc03
Ahlam <Ahlam.musemohamed@gmail.com>
2025-08-08 16:04:07 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### what i used
+
+- react router
+
+### Notes
+- to make sure pages have both the navbar and footer components, we use React router dom Outlet
---
+++ src/pages/Write.jsx
@@ -0,0 +1,11 @@
+// a single post page of a blog
+
+import React from 'react'
+
+const Write = () => {
+  return (
+    <div>Write</div>
+  )
+}
+
+export default Write
