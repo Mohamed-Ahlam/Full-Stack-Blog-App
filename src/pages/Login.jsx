061caa3dc5a9c5c6fb33944c2d18fea27218b81d
Ahlam <Ahlam.musemohamed@gmail.com>
2025-08-08 17:19:25 -0500

    completed login and register front-end

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className="auth">
       <h1>Login</h1>
 
-      <form>
+      <form className="form">
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
-        <button>Login</button>
+        <button className="primary--btn">Login</button>
+        <p>This is an error</p>
+        <span>Don't have an account? <Link to='/register'>Register</Link></span>
       </form>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="auth">
+      <h1>Register</h1>
 
-export default Register
+      <form className="form">
+        <input type="text" placeholder="username" />
+        <input type="email" placeholder="email" required />
+        <input type="password" placeholder="password" />
+        <button className="primary--btn">Register</button>
+        <p>This is an error!</p>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
